f4523c3bb94b902c18e9ba694f74122c5082bef2
Irappa Pattar <irappapattar30@gmail.com>
2017-10-27 15:23:10 +0530

    testing
    Refactored the code of Myproject
    
    moved src and pkg file outside Myproject directory
    
    removed tab from config.yml
    
    print statement,getlen,test
    
    get length
    
    test

--- Hello/Helloworld.go
+++ Hello/Helloworld.go
@@ -5,6 +5,10 @@ import "fmt"
 func Helloworld() {
 	total := hello()
 	fmt.Println("Total", total)
+	fmt.Println("end of statement")
+	l := getLenofString("hello")
+	fmt.Println(l)
+	sayHello()
 }
 
 func hello() int {
@@ -12,5 +16,12 @@ func hello() int {
 	for i := 0; i < 5; i++ {
 		total += i
 	}
+	fmt.Println("total", total)
 	return total
+}
+func getLenofString(s string) int {
+	return len(s)
+}
+func sayHello() {
+	fmt.Println("hello")
 }
